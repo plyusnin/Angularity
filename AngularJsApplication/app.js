898929f72a93801941f3c49e7be4034421562b9a
Plyusnin <eninbox@gmail.com>
2015-06-12 16:56:18 +0500

    Список, репеатер

--- AngularJsApplication/app.js
+++ AngularJsApplication/app.js
@@ -6,4 +6,9 @@ myApp.controller("MainCtrl", function($scope) {
         name: "Евгений",
         login: "paboka"
     };
+    $scope.programs = [
+        { name: "CanProg", version: "1.0" },
+        { name: "FmPack", version: "1.4" },
+        { name: "Burner", version: "1.1" }
+    ];
 });
