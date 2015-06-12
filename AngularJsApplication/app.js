e9836bc4952cd193c109326bd45698ad7d3aa56a
Plyusnin <eninbox@gmail.com>
2015-06-12 16:52:29 +0500

    Первая ангулярность

---
+++ AngularJsApplication/app.js
@@ -0,0 +1,9 @@
+﻿var myApp = angular.module("myApp", []);
+
+
+myApp.controller("MainCtrl", function($scope) {
+    $scope.userDetails = {
+        name: "Евгений",
+        login: "paboka"
+    };
+});
