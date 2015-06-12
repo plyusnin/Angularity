663e9697b88e379dab183fcbcc44ffaa16d35101
Plyusnin <eninbox@gmail.com>
2015-06-12 19:42:30 +0500

    Прикрутил бутстраповские стили

--- AngularJsApplication/app.js
+++ AngularJsApplication/app.js
@@ -7,8 +7,8 @@ myApp.controller("MainCtrl", function($scope) {
         login: "paboka"
     };
     $scope.programs = [
-        { name: "CanProg", version: "1.0" },
-        { name: "FmPack", version: "1.4" },
-        { name: "Burner", version: "1.1" }
+        { name: "CanProg", version: "1.0", description: "Программа для внутрисистемной прошивки ячеек комплекса БЛОК по CAN-линии." },
+        { name: "FmPack", version: "1.4", description: "Программа для создания sfp-файлов с прошивками." },
+        { name: "Burner", version: "1.1", description: "Программа для первичной прошивки ячеек комплекса БЛОК." }
     ];
 });
