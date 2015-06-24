c370511dcd93e2e184170f1e3291ceda87c81801
Plyusnin <eninbox@gmail.com>
2015-06-25 03:21:09 +0500

    Добавлена поддержка сохранения номера последней увиденной версии в Cookies

--- AngularJsApplication/app.js
+++ AngularJsApplication/app.js
@@ -1,18 +1,39 @@
-﻿var myApp = angular.module("myApp", []);
+﻿var myApp = angular.module("myApp", ["ngCookies"]);
 
 
-myApp.controller("MainCtrl", function($scope) {
+myApp.controller("MainCtrl", function ($scope, $cookies) {
+    var versions = $cookies.getObject("lastSeenVersion");
+    var compareVersions = function (v1, v2) {
+        if (v1.major > v2.major) return 1;
+        if (v1.major < v2.major) return -1;
+        if (v1.minor > v2.minor) return 1;
+        if (v1.minor < v2.minor) return -1;
+        return 0;
+    }
+
     $scope.userDetails = {
         name: "Евгений",
         login: "paboka"
     };
     $scope.programs = [
-        { name: "CanProg", version: "1.0", description: "Программа для внутрисистемной прошивки ячеек комплекса БЛОК по CAN-линии." },
-        { name: "FmPack", version: "1.4", description: "Программа для создания sfp-файлов с прошивками." },
-        { name: "Burner", version: "1.1", description: "Программа для первичной прошивки ячеек комплекса БЛОК." }
+        { name: "CanProg", version: { major: 0, minor: 9 }, description: "Программа для внутрисистемной прошивки ячеек комплекса БЛОК по CAN-линии." },
+        { name: "FmPack", version: { major: 1, minor: 4 }, description: "Программа для создания sfp-файлов с прошивками." },
+        { name: "Burner", version: { major: 1, minor: 1 }, description: "Программа для первичной прошивки ячеек комплекса БЛОК." }
     ];
     $scope.selectedProgram = $scope.programs[0];
     $scope.selectProgram = function (program) {
         $scope.selectedProgram = program;
+        if (versions == undefined)
+            versions = {};
+        versions[program.name] = program.version;
+        $cookies.putObject("lastSeenVersion", versions);
     };
+    $scope.isNewVersion = function (programName, version) {
+        if (versions == undefined)
+            return true;
+        if (versions[programName] == undefined)
+            return true;
+        return compareVersions(version, versions[programName]) > 0;
+    }
+
 });
